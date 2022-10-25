ff8fa826d5d94d35f6b6792835d52e75da914cfb
niperezv <47955066+niperezv@users.noreply.github.com>
2022-10-25 17:28:27 -0500

    FinalJugador

---
+++ src/main/java/Jugador.java
@@ -0,0 +1,33 @@
+package futbol;
+
+public class Jugador extends Futbolista {
+	public short golesMarcados;
+	public byte dorsal;
+
+	public Jugador(String nombre, int edad, String posicion, short golesMarcados, byte dorsal) {
+		super(nombre, edad, posicion);
+		this.golesMarcados = golesMarcados;
+		this.dorsal = dorsal;
+	}
+
+	public Jugador() {
+		super();
+		golesMarcados = 289;
+		dorsal = 7;
+	}
+
+	@Override
+	public int compareTo(Object futbol) {
+		Jugador player = (Jugador) futbol; // Casteo de futbolista a jugador
+		return this.getEdad() - player.getEdad();
+	}
+
+	public boolean jugarConLasManos() {
+		return false;
+	}
+
+	public String toString() {
+		return "El futbolista " + getNombre() + " tiene " + getEdad() + ", y juega de " + getPosicion()
+				+ " con el dorsal " + this.dorsal + ". Ha marcado " + this.golesMarcados;
+	}
+}
